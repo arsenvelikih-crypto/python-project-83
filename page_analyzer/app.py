0bcaf3b837fc9edf34f13a49216e5769dcfd6316
arsenvelikih-crypto <arsenvelikih@gmail.com>
2026-07-30 15:55:26 +0400

    first steps

---
+++ page_analyzer/app.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from flask import (
+    Flask,
+    render_template,
+)
+
+load_dotenv()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+
+
+
+
+
+
+
+
+@app.route("/")
+def index():
+    return render_template("home.html")
